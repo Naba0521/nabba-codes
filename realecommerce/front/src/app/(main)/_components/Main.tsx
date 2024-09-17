9ade3ba26907c25a29f0440c810378a174084af8
24LP8045 24LP8045 <24LP8045@G301-CC0209.local>
2024-09-17 13:16:49 +0800

    asdasdasd

--- realecommerce/front/src/app/(main)/_components/Main.tsx
+++ realecommerce/front/src/app/(main)/_components/Main.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { BHeart } from "@/assets/BHeart";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MainProductCard } from "./MainProductCard";
+import axios from "axios";
+import { log } from "console";
 const data = [
   { img: "/2.png", title: "The Prompt Magazine", price: 120000 },
   { img: "/3.png", title: "Chunky Glyph Tee", price: 120000 },
@@ -23,18 +25,47 @@ const data = [
   { img: "/3.png", title: "Chunky Glyph Tee", price: 120000 },
   { img: "/4.png", title: "All Smiles Nalgene", price: 120000 },
 ];
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string[];
+}
+
+interface ProductsResponse {
+  products: Product[]; // This represents the `products.products` structure
+}
 export const Main = () => {
-  // const [savedHeart, setSavedHeart] = useState(false);
+  const [products, setProducts] = useState<ProductsResponse[]>([]);
+  const getProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/product");
+      setProducts(response.data);
+      // console.log("1111asdsasd", response.data.products[0].image[0]);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div className="w-full flex flex-col items-center py-11 px-[200px] gap-11">
       <div className="relative w-[1040px] h-[446px] rounded-2xl overflow-hidden">
-        <Image
-          quality={100}
-          src="/1.png"
-          fill
-          alt="a"
-          className="object-cover"
-        />
+        {products.products ? (
+          <Image
+            quality={100}
+            src={products.products[0].image[0]} // Ensure the image array exists
+            fill
+            alt="a"
+            className="object-cover"
+          />
+        ) : (
+          <div className="flex justify-center items-center h-full text-3xl font-bold">
+            Loading...
+          </div> // You can replace this with a skeleton loader or any placeholder
+        )}
         <div className="flex flex-col absolute bottom-8 left-8">
           <div className="text-lg">Wildflower Hoodie</div>
           <div className="text-4xl font-bold">120’000₮</div>
