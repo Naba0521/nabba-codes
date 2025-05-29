67fdb72c9b5556a8c352920034648fabec0cb829
Naba0521 <naranbaatar.m@nest.edu.mn>
2025-05-29 14:32:17 +0800

    test2

--- realecommerce/api/src/controllers/foosBalll/create-foosball.controller.ts
+++ realecommerce/api/src/controllers/foosBalll/create-foosball.controller.ts
@@ -9,11 +9,11 @@ export const createFoosballController: RequestHandler = async (req, res) => {
       createdAt: new Date(),
       updatedAt: new Date(),
     });
-    return res.status(201).json({
+    res.status(201).json({
       message: "memberNames nemegdsen",
     });
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       message: "memberNames buruu l nemeed bndaa",
     });
   }
--- realecommerce/api/src/controllers/foosBalll/get-foosball.controller.ts
+++ realecommerce/api/src/controllers/foosBalll/get-foosball.controller.ts
@@ -1,14 +1,14 @@
 import { RequestHandler } from "express";
 import { foosBallModel } from "../../models/foosBall.schema";
 
-export const getFoosballController: RequestHandler = async (req, res) => {
+export const getFoosballController: RequestHandler = async (_req, res) => {
   try {
     const memberNames = await foosBallModel.find({});
-    return res.status(200).json({
+    res.status(200).json({
       memberNames,
     });
   } catch (error) {
-    return res.status(500).json({
+    res.status(500).json({
       message: "memberNames Awahad asuudaltai l bndaa",
     });
   }
